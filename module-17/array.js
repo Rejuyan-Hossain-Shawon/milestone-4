f19cf6cdd74c3c80551607fa9a3df1295c309dac
RejuyanHossainShawon <76831441+RejuyanHossainShawon@users.noreply.github.com>
2021-08-08 03:57:03 +0600

    have to finish many things

---
+++ module-17/array.js
@@ -0,0 +1,24 @@
+var name = ['shawon', 'aksh', 'moon', 'nahid', 'sani'];
+console.log(name);
+
+// push and pop method 
+name.push("agar");
+console.log(name);
+name.pop();
+name.pop();
+console.log(name);
+// shift and unshift method
+
+
+var shifted = name.shift();
+console.log(name);
+name.unshift('rozer');
+console.log(name);
+console.log(shifted);
+var nums = [1, 5, 8];
+
+
+var n = nums.indexOf(0)
+var n2 = nums.indexOf(1);
+console.log(n, n2);
+console.log(6 >= 6);
